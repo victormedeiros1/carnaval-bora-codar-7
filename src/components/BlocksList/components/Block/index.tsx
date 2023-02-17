e2bba1bd6cffd0d66c70a83753e226a90760cafd
José Victor <josevictordev@gmail.com>
2023-02-17 17:14:22 -0300

    feat: BlockList and Block component

--- src/components/BlocksList/components/Block/index.tsx
+++ src/components/BlocksList/components/Block/index.tsx
@@ -1,18 +1,22 @@
 interface Props {
-  title: string;
+  name: string;
   cover: string;
   description: string;
   location: string;
 }
 
-export default function Block({ title, description, cover, location }: Props) {
+export default function Block({ name, description, cover, location }: Props) {
   return (
-    <article className="bg-white">
+    <article className="bg-white border border-[#EAEAEA] rounded-10 ">
       <div>
-        <img className="m-0" src={cover} alt={`Bloco ${title}`} />
+        <img
+          className="rounded-t-[10px] mr-0"
+          src={cover}
+          alt={`Bloco ${name}`}
+        />
       </div>
-      <div>
-        <h3 className="text-md font-bold">{title}</h3>
+      <div className="flex flex-col gap-4 p-4">
+        <h3 className="text-md font-bold">{name}</h3>
         <h4 className="text-sm">{description}</h4>
         <h5 className="text-sm">{location}</h5>
       </div>
--- src/components/BlocksList/index.tsx
+++ src/components/BlocksList/index.tsx
@@ -3,28 +3,28 @@ import Block from "./components/Block";
 const blocks = [
   {
     id: 0,
-    title: "Theme só res enha",
+    name: "Theme só res enha",
     cover: "/src/assets/images/bloco-1.jpg",
     description: "Lorem ipsum dolot sit amet lorem ipsum dolot sit amet",
     location: "Natal - RN",
   },
   {
     id: 1,
-    title: "Não sou deSASS coisas",
+    name: "Não sou deSASS coisas",
     cover: "/src/assets/images/bloco-2.jpg",
     description: "Lorem ipsum dolot sit amet lorem ipsum dolot sit amet",
     location: "Caicó - RN",
   },
   {
     id: 2,
-    title: "Assets te pego",
+    name: "Assets te pego",
     cover: "/src/assets/images/bloco-3.jpg",
     description: "Lorem ipsum dolot sit amet lorem ipsum dolot sit amet",
     location: "Salvador - BA",
   },
   {
     id: 3,
-    title: "As gata e IML",
+    name: "As gata e IML",
     cover: "/src/assets/images/bloco-4.jpg",
     description: "Lorem ipsum dolot sit amet lorem ipsum dolot sit amet",
     location: "Péssimo - MC",
@@ -33,15 +33,15 @@ const blocks = [
 
 export default function BlocksList() {
   return (
-    <section className="max-w-[78.5rem] mx-auto my-0">
+    <section className="max-w-[78.5rem] mx-auto my-0 p">
       <header className="mb-10 mt-24">
         <h3 className="text-lg font-bold">Blocos recomendados</h3>
       </header>
       <div className="grid grid-cols-3 gap-8">
-        {blocks.map(({ id, title, description, cover, location }) => (
+        {blocks.map(({ id, name, description, cover, location }) => (
           <Block
             key={id}
-            title={title}
+            name={name}
             cover={cover}
             description={description}
             location={location}
