be986a683ca876111bf986261f7a89de43ee7ffa
José Victor <josevictordev@gmail.com>
2023-02-23 18:42:44 -0300

    feat: getBlocks from API

--- src/components/BlocksList/index.tsx
+++ src/components/BlocksList/index.tsx
@@ -1,44 +1,26 @@
+import { useEffect, useState } from "react";
 import Block from "./components/Block";
+import { Block as BlockTypes } from "../../types/block";
 
-const blocks = [
-  {
-    id: 0,
-    name: "Theme só res enha",
-    cover: "/src/assets/images/bloco-1.jpg",
-    description: "Lorem ipsum dolot sit amet lorem ipsum dolot sit amet",
-    location: "Natal - RN",
-  },
-  {
-    id: 1,
-    name: "Não sou deSASS coisas",
-    cover: "/src/assets/images/bloco-2.jpg",
-    description: "Lorem ipsum dolot sit amet lorem ipsum dolot sit amet",
-    location: "Caicó - RN",
-  },
-  {
-    id: 2,
-    name: "Assets te pego",
-    cover: "/src/assets/images/bloco-3.jpg",
-    description: "Lorem ipsum dolot sit amet lorem ipsum dolot sit amet",
-    location: "Salvador - BA",
-  },
-  {
-    id: 3,
-    name: "As gata e IML",
-    cover: "/src/assets/images/bloco-4.jpg",
-    description: "Lorem ipsum dolot sit amet lorem ipsum dolot sit amet",
-    location: "Péssimo - MC",
-  },
-];
+const url =
+  "http://bora-codar-challenge-7-backend-production.up.railway.app/blocks";
 
 export default function BlocksList() {
+  const [blocks, setBlocks] = useState<BlockTypes[]>([]);
+
+  useEffect(() => {
+    fetch(url)
+      .then((response) => response.json())
+      .then(({ blocks }) => setBlocks(blocks));
+  }, []);
+
   return (
     <section className="max-w-[78.5rem] mx-auto my-0 p">
       <header className="mb-10 mt-24">
         <h3 className="text-lg font-bold">Blocos recomendados</h3>
       </header>
       <div className="grid grid-cols-3 gap-8">
-        {blocks.map(({ id, name, description, cover, location }) => (
+        {blocks?.map(({ id, name, description, cover, location }) => (
           <Block
             key={id}
             name={name}
