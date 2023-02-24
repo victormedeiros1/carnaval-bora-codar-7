df1107f42fcfe9a7ad04acf24e5a0e16e315a96f
José Victor <josevictordev@gmail.com>
2023-02-24 17:39:20 -0300

    feat: input colors gray-700

--- src/components/Search/index.tsx
+++ src/components/Search/index.tsx
@@ -7,7 +7,7 @@ export default function Search() {
       <div className="w-full max-w-348 flex bg-[#F4F4FF] rounded">
         <MapPinIcon className="h-6 w-6 text-red m-3.5 my-auto" />
         <input
-          className="w-full bg-transparent px-3.5 py-3"
+          className="w-full text-gray-00 bg-transparent px-3.5 py-3"
           type="text"
           placeholder="Pesquisar por nome"
           aria-placeholder="Pesquisar por nome"
@@ -16,7 +16,10 @@ export default function Search() {
 
       <div className="w-full max-w-348 bg-[#F4F4FF] flex rounded">
         <MagnifyingGlassIcon className="h-6 w-6 text-red mx-3.5 my-auto" />
-        <select className="w-full bg-transparent px-3.5 py-3" name="cities">
+        <select
+          className="w-full text-gray-700 bg-transparent px-3.5 py-3"
+          name="cities"
+        >
           <option value="Bloco 1">Bloco 1</option>
           <option value="Bloco 2">Bloco 2</option>
           <option value="Bloco 3">Bloco 3</option>
