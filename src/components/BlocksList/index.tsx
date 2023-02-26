4cb90fe6a0478627ff1ff636786b45baa786dfd5
José Victor <josevictordev@gmail.com>
2023-02-26 15:03:56 -0300

    feat: handleSubmit form to filter blocks

--- src/components/BlocksList/index.tsx
+++ src/components/BlocksList/index.tsx
@@ -1,12 +1,15 @@
 import { useEffect } from "react";
 import { useBlocks } from "../../hooks/useBlocks";
+import { useFilteredBlocks } from "../../hooks/useFilteredBlocks";
 import Block from "./components/Block";
+import Loading from "../Loading";
 
 const url =
   "http://bora-codar-challenge-7-backend-production.up.railway.app/blocks";
 
 export default function BlocksList() {
   const { blocks, setBlocks } = useBlocks();
+  const { filteredBlocks } = useFilteredBlocks();
 
   useEffect(() => {
     fetch(url)
@@ -14,21 +17,35 @@ export default function BlocksList() {
       .then(({ blocks }) => setBlocks(blocks));
   }, []);
 
+  if (!blocks) return <Loading />;
+
   return (
     <section className="max-w-[78.5rem] mx-auto my-0 p">
       <header className="mb-10 mt-24">
         <h3 className="text-lg font-bold">Blocos recomendados</h3>
       </header>
       <div className="grid grid-cols-3 gap-8">
-        {blocks?.map(({ id, name, description, cover, location }) => (
-          <Block
-            key={id}
-            name={name}
-            cover={cover}
-            description={description}
-            location={location}
-          />
-        ))}
+        {filteredBlocks.length > 0
+          ? filteredBlocks?.map(
+              ({ id, name, description, cover, location }) => (
+                <Block
+                  key={id}
+                  name={name}
+                  cover={cover}
+                  description={description}
+                  location={location}
+                />
+              )
+            )
+          : blocks?.map(({ id, name, description, cover, location }) => (
+              <Block
+                key={id}
+                name={name}
+                cover={cover}
+                description={description}
+                location={location}
+              />
+            ))}
       </div>
     </section>
   );
--- src/components/Search/index.tsx
+++ src/components/Search/index.tsx
@@ -2,6 +2,7 @@ import { MagnifyingGlassIcon, MapPinIcon } from "@heroicons/react/24/outline";
 import Button from "../Button";
 
 import { useBlocks } from "../../hooks/useBlocks";
+import { useFilteredBlocks } from "../../hooks/useFilteredBlocks";
 
 const locations = [
   {
@@ -23,45 +24,49 @@ const locations = [
 ];
 
 export default function Search() {
-  const { blocks, setBlocks } = useBlocks();
+  const { blocks } = useBlocks();
+  const { setFilteredBlocks } = useFilteredBlocks();
 
-  const handleTextField = (e: React.ChangeEvent<HTMLInputElement>) => {
-    const text = e.target.value;
+  const handleSubmit = (e: any) => {
+    e.preventDefault();
+    const text = e.target.ele;
     const filteredBlocks = blocks.filter((block) => block.name.includes(text));
 
-    setBlocks(filteredBlocks);
+    setFilteredBlocks(filteredBlocks);
+    console.log("FILTERED BLOCKS", filteredBlocks);
   };
 
   return (
-    <div className="z-10 w-full max-w-[62rem] bg-white border border-[#EAEAEA] rounded-[10px] flex gap-6 p-10">
-      <div className="w-full max-w-348 flex bg-[#F4F4FF] rounded">
-        <MagnifyingGlassIcon className="h-6 w-6 text-red mx-3.5 my-auto" />
+    <div className="z-10 w-full max-w-[62rem] bg-white border border-[#EAEAEA] rounded-[10px]  p-10">
+      <form className="flex gap-6" onSubmit={handleSubmit}>
+        <div className="w-full max-w-348 flex bg-[#F4F4FF] rounded">
+          <MagnifyingGlassIcon className="h-6 w-6 text-red mx-3.5 my-auto" />
 
-        <input
-          onChange={handleTextField}
-          className="w-full text-gray-700 bg-transparent px-3.5 py-3"
-          type="text"
-          placeholder="Pesquisar por nome"
-          aria-placeholder="Pesquisar por nome"
-        />
-      </div>
+          <input
+            className="w-full text-gray-700 bg-transparent px-3.5 py-3"
+            type="text"
+            name="text"
+            placeholder="Pesquisar por nome"
+          />
+        </div>
 
-      <div className="w-full max-w-348 bg-[#F4F4FF] flex rounded">
-        <MapPinIcon className="h-6 w-6 text-red m-3.5 my-auto" />
+        <div className="w-full max-w-348 bg-[#F4F4FF] flex rounded">
+          <MapPinIcon className="h-6 w-6 text-red m-3.5 my-auto" />
 
-        <select
-          className="w-full text-gray-700 bg-transparent px-3.5 py-3"
-          name="cities"
-        >
-          {locations.map(({ id, name }) => (
-            <option key={id} value={name}>
-              {name}
-            </option>
-          ))}
-        </select>
-      </div>
+          <select
+            className="w-full text-gray-700 bg-transparent px-3.5 py-3"
+            name="locations"
+          >
+            {locations.map(({ id, name }) => (
+              <option key={id} value={name}>
+                {name}
+              </option>
+            ))}
+          </select>
+        </div>
 
-      <Button>BUSCAR AGORA</Button>
+        <Button>BUSCAR AGORA</Button>
+      </form>
     </div>
   );
 }
