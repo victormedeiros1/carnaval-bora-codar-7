5a7ffe6a9a3547ff0a27287e4388828a108415b0
José Victor <josevictordev@gmail.com>
2023-02-27 15:26:53 -0300

    fix: import images

--- src/helpers/slugs.ts
+++ src/helpers/slugs.ts
@@ -1,32 +1,36 @@
 import { Block } from "../types/block";
 import { Location } from "../types/location";
+import block1 from "/src/assets/images/bloco-1.jpg";
+import block2 from "/src/assets/images/bloco-2.jpg";
+import block3 from "/src/assets/images/bloco-3.jpg";
+import block4 from "/src/assets/images/bloco-4.jpg";
 
 export const blockOptions: Block[] = [
   {
     id: 0,
     name: "Theme só res enha",
-    cover: "/assets/images/bloco-1.jpg",
+    cover: block1,
     description: "Lorem ipsum dolot sit amet lorem ipsum dolot sit amet",
     location: "Natal - RN",
   },
   {
     id: 1,
     name: "Não sou deSASS coisas",
-    cover: "/assets/images/bloco-2.jpg",
+    cover: block2,
     description: "Lorem ipsum dolot sit amet lorem ipsum dolot sit amet",
     location: "Caicó - RN",
   },
   {
     id: 2,
     name: "Assets te pego",
-    cover: "/assets/images/bloco-3.jpg",
+    cover: block3,
     description: "Lorem ipsum dolot sit amet lorem ipsum dolot sit amet",
     location: "Salvador - BA",
   },
   {
     id: 3,
     name: "As gata e IML",
-    cover: "./assets/images/bloco-4.jpg",
+    cover: block4,
     description: "Lorem ipsum dolot sit amet lorem ipsum dolot sit amet",
     location: "Péssimo - MC",
   },
