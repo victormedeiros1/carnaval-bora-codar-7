3fd77f6841f4a269f875b1279aa7d07b71fe52e7
José Victor <josevictordev@gmail.com>
2023-02-28 19:44:46 -0300

    feat: added more 3 blocks

--- src/helpers/slugs.ts
+++ src/helpers/slugs.ts
@@ -6,6 +6,9 @@ import block3 from "/src/assets/images/bloco-3.jpg";
 import block4 from "/src/assets/images/bloco-4.jpg";
 import block5 from "/src/assets/images/bloco-5.jpg";
 import block6 from "/src/assets/images/bloco-6.jpg";
+import block7 from "/src/assets/images/bloco-7.jpg";
+import block8 from "/src/assets/images/bloco-8.jpg";
+import block9 from "/src/assets/images/bloco-9.jpg";
 
 export const blockOptions: Block[] = [
   {
@@ -31,24 +34,45 @@ export const blockOptions: Block[] = [
   },
   {
     id: 3,
+    name: "Todo mundo null",
+    cover: block7,
+    description: "Lorem ipsum dolot sit amet lorem ipsum dolot sit amet",
+    location: "Florianópolis - SC",
+  },
+  {
+    id: 4,
     name: "As gata e IML",
     cover: block4,
     description: "Lorem ipsum dolot sit amet lorem ipsum dolot sit amet",
     location: "Péssimo - MC",
   },
   {
-    id: 4,
+    id: 5,
     name: "Amigos do P Érri",
     cover: block5,
     description: "Lorem ipsum dolot sit amet lorem ipsum dolot sit amet",
     location: "Péssimo - MC",
   },
   {
-    id: 5,
+    id: 6,
     name: "Sou inocente seu NPM",
     cover: block6,
     description: "Lorem ipsum dolot sit amet lorem ipsum dolot sit amet",
     location: "Péssimo - MC",
+  },
+  {
+    id: 7,
+    name: "Manda node",
+    cover: block8,
+    description: "Lorem ipsum dolot sit amet lorem ipsum dolot sit amet",
+    location: "Teresina - PI",
+  },
+  {
+    id: 8,
+    name: "EnCACHE aqui",
+    cover: block9,
+    description: "Lorem ipsum dolot sit amet lorem ipsum dolot sit amet",
+    location: "Brasília - DF",
   },
 ];
 
